db1e06de62b9a6512eae1b5d368870b3f539720f
Reder <reder.ac3@gmail.com>
2022-06-27 00:48:29 +0200

    Fixed script for adding a printer

--- install.js
+++ install.js
@@ -7,15 +7,24 @@ import axios from 'axios';
 import generateUrlPrinter from './functions/generateUrlPrinter.js'
 
 const receiver = async () => {
+
     const { code } = await inquirer.prompt([
         { type: 'input', name: 'code', message: 'Podaj kod użytkownika:' }
     ]);
-    const user = await axios.get(`https://bdo.rdnt.pl/users/${code}`);
-    if (user) {
-        await axios.put(`https://bdo.rdnt.pl/users/${user.id}`, { pritner: generateUrlPrinter()});
-    }
 
-    console.log('Konfiguracja przebiegła pomyślnie. \nDrukarka dodana do konta.');
+    const user = await axios.get(`http://localhost:3000/users/${code}`);
+
+    if (user.data.status === 'success') {
+        try {
+            await axios.put(`http://localhost:3000/users/${user.data.data.id}`, { printer: generateUrlPrinter() });
+            console.log('Konfiguracja przebiegła pomyślnie. \nDrukarka dodana do konta.');
+
+        } catch (error) {
+            console.log('Wystąpił błąd. Spróbuj ponownie.')
+
+        }
+
+    } else console.log('Kod niepoprawny! Podaj kod wygenerowany w aplikacji "Ustawienia > Drukarki > Dodaj drukarkę"')
 
 }
 
